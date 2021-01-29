700f5ec9450a2d624b019d707a00345e7f012346
unknown <kasun7153@gmail.com>
2021-01-29 15:40:16 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require('cors')
+const nodemailer = require("nodemailer");
+const bodyParser = require('body-parser')
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+app.use(bodyParser.json());
+
+
+const  transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, 
+    auth: {
+      user: "app.nodemailer@gmail.com", 
+      pass: "Nodemailer123", 
+    },
+  });
+
+app.post("/api/v1",(req,res)=>{
+    console.log(req.body)
+
+    transporter.sendMail({
+        from: req.body.from+"<app.nodemailer@gmail.com>", 
+        to: req.body.to,
+        subject: req.body.subject, 
+        html: req.body.html
+      }).then((respond)=>{
+        console.log(respond)
+        res.json({
+            sucess:1,
+            message:"Email sent",
+        })
+      }).catch(err=>{
+          console.log(err)
+          res.json({
+            sucess:0,
+            message:"Email sending failed",
+            error:err.message
+        })
+      });
+})
+
+app.listen(process.env.APP_PORT||3000,()=>{
+    console.log("Server is running on",process.env.APP_PORT||3000)
+})
