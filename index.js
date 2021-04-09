f5a203fe8fad26e15eb1de1440a148b9de3c8820
unknown <kasun7153@gmail.com>
2021-04-09 11:03:45 +0530

    make it better

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const  transporter = nodemailer.createTransport({
   });
 
   app.get("/",(req,res)=>{
-      res.send("Email sending Node server by Kasun")
+      res.send("Email sending Node server by Me")
 })
 
 app.post("/api/v1",(req,res)=>{
